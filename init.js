7b13c185bfdc57d54cfc755bd77d8c5319728d8d
YiJun <cloudywolf1@qq.com>
2019-09-10 09:17:23 +0800

    update init template

--- init.js
+++ init.js
@@ -1,10 +1,16 @@
 
-module.exports = function(context, args){
-  // Extra context
-  //   - context.projectName
-  //   - context.projectRoot -> context.utils.paths.currentPath
-
+// Extra context
+//   - context.projectName
+//   - context.projectRoot -> context.utils.paths.currentPath
+//
+const demo2 = function(context, args) {
   const { std } = context.utils;
-
   std.warn("implement [xcli init]");
+};
+
+module.exports = {
+  "[XPT] TEMPLATE DEMO": {
+    "[XPT] DEMO1": "DemonCloud/xcli-plugin-template",
+    "[XPT] DEMO2": demo2
+  }
 };
